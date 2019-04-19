89d4ca536d37bb810c3ebe86ab322534dca5b90d
Tom Culpepper <tculpepp@gmail.com>
2019-04-19 18:37:31 -0400

    Still trying on ps1c.  Added a demo file

--- 6.0001/psa1b.py
+++ 6.0001/psa1b.py
@@ -23,8 +23,8 @@ total_cost = int(input("Enter the cost of your dream home $"))
 semi_annual_raise = float(input("Enter the semi­annual raise, as a decimal: "))
 
 while current_savings < (total_cost * portion_down_payment):
-    current_savings = current_savings + (annual_salary/12) * portion_saved + (current_savings*r/12)
-    month_counter = month_counter + 1
+    current_savings += (annual_salary/12) * portion_saved + (current_savings*r/12)
+    month_counter += 1
     if month_counter % semi_annual_raise_months == 0:
-        annual_salary = annual_salary + (annual_salary * semi_annual_raise)
+        annual_salary += (annual_salary * semi_annual_raise)
 print ("It will take", month_counter, "months to save up a down payment for your dream house.")
